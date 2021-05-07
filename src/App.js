8658ebc35ff9153d5ccfb6c7cd3426a3334507e5
Thushara Thiwanka <thusharathiwanka123@gmail.com>
2021-05-07 15:10:23 +0530

    CREATE: search functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,33 +1,50 @@
 import React, { useState, useEffect } from "react";
 import Form from "./components/Form";
-import Recipe from "./Recipe";
+import Recipe from "./components/Recipe";
 
 function App() {
-  const API_ID = `9f14300c`;
+  const APP_ID = `9f14300c`;
   const API_KEY = `9b2a978dc02f1fb030f83503dcd21795`;
 
   const [recipes, setRecipes] = useState([]);
+  const [search, setSearch] = useState("");
+  const [query, setQuery] = useState("chicken");
 
   useEffect(() => {
     getRecipes();
-  }, []);
+  }, [query]);
 
   const getRecipes = async () => {
     const response = await fetch(
-      `https://api.edamam.com/search?q=chicken&app_id=${API_ID}&app_key=${API_KEY}`
+      `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${API_KEY}`
     );
     const data = await response.json();
     console.log(data);
     setRecipes(data.hits);
   };
 
+  const handleQuery = (e) => {
+    e.preventDefault();
+    setQuery(search);
+  };
+
+  const handleQueryChange = (e) => {
+    setSearch(e.target.value);
+  };
+
   return (
-    <>
-      <Form />
-      {recipes.map((recipe) => (
-        <Recipe recipe={recipe} key={recipe.index} />
-      ))}
-    </>
+    <div className="container">
+      <Form
+        search={search}
+        handleQueryChange={handleQueryChange}
+        handleQuery={handleQuery}
+      />
+      <div className="cards-container">
+        {recipes.map((recipe) => (
+          <Recipe recipe={recipe} key={recipe.index} />
+        ))}
+      </div>
+    </div>
   );
 }
 
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,12 +1,18 @@
 import React from "react";
 import { RiSearchLine } from "react-icons/ri";
 
-const Form = () => {
+const Form = ({ search, handleQueryChange, handleQuery }) => {
   return (
     <div className="form-container">
       <form>
-        <input type="text" name="search-query" id="search-query" />
-        <button type="submit">
+        <input
+          type="text"
+          name="search-query"
+          id="search-query"
+          value={search}
+          onChange={(e) => handleQueryChange(e)}
+        />
+        <button type="submit" onClick={(e) => handleQuery(e)}>
           <RiSearchLine />
         </button>
       </form>
